f08b949b64283393b54ef9fbe4bc224a203b9dd4
TahirAnny <tahiraAnny@gmail.com>
2022-02-19 22:28:35 +0600

    print two integers from the list in one line problem solution added

---
+++ Random/01 Comma Separated Integers/01 ProblemStatement.md
@@ -0,0 +1,12 @@
+#### Write a program that will print two integers from the list in one line with a comma between them and then another line and so on.
+
+    Sample Input 1:12 15
+    Sample Output 1: 
+    12, 15
+
+
+    Sample Input 2: 4 5 10 11 13
+    Sample Output 2:
+    4, 5
+    10, 11
+    13
---
+++ Random/01 Comma Separated Integers/SeparatedInteger/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SeparatedInteger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var a = new int[] { 12, 13, 14, 15, 16, 17, 18, 7, 96 };
+            var newList = new List<int>();
+
+            var data = "";
+
+            for (var i = 0; i < a.Length; i++)
+            {
+                newList.Add(a[i]);
+
+                //Solution 1
+                if (newList.Count == 2 || i == (a.Length - 1))
+                {
+                    Console.WriteLine(string.Join(",", newList));
+                    newList.Clear();
+                }
+
+                //Solution 2
+                if (newList.Count == 2 || i == (a.Length - 1))
+                {
+                    data = data + String.Join(",", newList) + "\n";
+                    newList.Clear();
+                }
+            }
+            Console.WriteLine(data);
+        }
+    }
+}
