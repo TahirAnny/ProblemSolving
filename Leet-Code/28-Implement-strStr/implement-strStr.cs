eb76fe7549d0650149b6bbc581b2e892e6f68f3b
TahirAnny <tahiraAnny@gmail.com>
2022-04-21 22:27:10 +0600

    leetcode 28 problem solution in c#

--- Leet-Code/28-Implement-strStr/implement-strStr.cs
+++ Leet-Code/28-Implement-strStr/implement-strStr.cs
@@ -7,10 +7,17 @@ namespace SeparatedInteger
     {
         static void Main(string[] args)
         {
-
-            
+            var result = StrStr("hello", "ll");
+            Console.WriteLine(result);
         }
-        
-        
+
+        private static int StrStr(string haystack, string needle)
+        {
+            if (needle == String.Empty) return 0;
+
+            if (haystack.Contains(needle)) return haystack.IndexOf(needle);
+
+            return -1;
+        }
     }
 }
