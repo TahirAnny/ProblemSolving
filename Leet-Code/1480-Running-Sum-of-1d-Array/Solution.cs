bb4e59e372c9367f0c951f2ee8d71153a0ed1178
Tahira Bishwas Anny <35254801+TahirAnny@users.noreply.github.com>
2022-06-01 20:24:19 +0600

    Create Solution.cs

---
+++ Leet-Code/1480-Running-Sum-of-1d-Array/Solution.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public int[] RunningSum(int[] nums) {
+        
+        if( nums == null || nums.Length == 0 )
+            return new int[0];
+        
+        for( int i = 1; i < nums.Length; i++ )
+            nums[i] += nums[i-1];
+        
+        return nums;
+    }
+}
+
+//With Linq
+ 
+public class Solution {
+    public int[] RunningSum(int[] nums) {
+        
+        int sum = 0;
+        return nums.Select(x => sum += x).ToArray();
+        
+    }
+}
